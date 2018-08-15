7c05aa52ed8723ba6d123d72fa28c8f348e0cefe
Steve Rayner <srayner02@gmail.com>
2018-08-15 20:31:44 +0100

    further tidying

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,78 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo-dark.png';
-import './App.css';
-import VideoList from '../VideoList/VideoList';
-import Player from '../Player/Player';
-import SearchBox from '../SearchBox/SearchBox';
-import { connect } from 'react-redux';
-import { updateList, setCurrentId, getPopularVideos, searchVideos } from '../../actions/videoActions';
-import youtube from '../../library/youtube';
+import React, { Component } from "react";
+import logo from "./logo-dark.png";
+import "./App.css";
+import VideoList from "../VideoList/VideoList";
+import Player from "../Player/Player";
+import SearchBox from "../SearchBox/SearchBox";
+import { connect } from "react-redux";
+import {
+  setCurrentId,
+  getPopularVideos,
+  searchVideos
+} from "../../actions/videoActions";
 
 class App extends Component {
+  searchHandler = text => {
+    this.props.searchVideos(text);
+  };
 
-    videoPlayerUrl = 'https://www.youtube.com/embed/';
-    
-    buildList = (response) => {
-        var items = response.data.items.map(item => {
-            return {
-                'id': item.id,
-                'thumb': item.snippet.thumbnails.default.url,
-                'title': item.snippet.title,
-                'channel': item.snippet.channelTitle,
-                'published': item.snippet.publishedAt,
-                'stats': item.statistics
-            };
-        });
-        this.props.updateList(items);
-    }
+  componentDidMount() {
+    this.props.getPopularVideos();
+  }
 
-    getVideos = (videoIds) => {
-        youtube.getVideos(videoIds).then(response => {
-            this.buildList(response);
-        });
-    }
-
-    searchHandler = (text) => {
-        this.props.searchVideos(text);
-    }
-
-    componentDidMount() {
-        this.props.getPopularVideos();
-    }
-
-    render() {
-        const currentSrc = this.videoPlayerUrl + this.props.video.currentId;
-        return (
-            <div className="App">
-                <header>
-                    <div>
-                        <img src={logo} className="logo" alt="logo" />
-                        <SearchBox onSubmit={this.searchHandler}/>
-                    </div>
-                    <Player src={currentSrc}/>
-                </header>
-                <main>
-                    <VideoList items={this.props.video.items} onClick={this.props.setCurrentId}/>
-                </main>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div>
+            <img src={logo} className="logo" alt="logo" />
+            <SearchBox onSubmit={this.searchHandler} />
+          </div>
+          <Player videoId={this.props.video.currentId} />
+        </header>
+        <main>
+          <VideoList
+            items={this.props.video.items}
+            onClick={this.props.setCurrentId}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        video: state.video,
-        playlist: state.playlist
-    };
-}
+const mapStateToProps = state => {
+  return {
+    video: state.video,
+    playlist: state.playlist
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        updateList: (items) => dispatch(updateList(items)),
-        setCurrentId: (id) => dispatch(setCurrentId(id)),
-        getPopularVideos: (response) => dispatch(getPopularVideos(response)),
-        searchVideos: (text) => dispatch(searchVideos(text))
-    };
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    setCurrentId: id => dispatch(setCurrentId(id)),
+    getPopularVideos: response => dispatch(getPopularVideos(response)),
+    searchVideos: text => dispatch(searchVideos(text))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,21 +1,19 @@
+import React from "react";
 
+const videoPlayerUrl = "https://www.youtube.com/embed/";
 
-import React from 'react';
-
-const Player = (props) => {
-    return (
-        <iframe
-            title="Player"
-            width="560"
-            height="315"
-            src={props.src}
-            frameBorder="0"
-            allow="autoplay; encrypted-media"
-            allowFullScreen>
-        </iframe>
-    );
-}
+const Player = props => {
+  return (
+    <iframe
+      title="Player"
+      width="560"
+      height="315"
+      src={videoPlayerUrl + props.videoId}
+      frameBorder="0"
+      allow="autoplay; encrypted-media"
+      allowFullScreen
+    />
+  );
+};
 
 export default Player;
-
-
